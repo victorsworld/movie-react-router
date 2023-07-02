11d64f4fcf36c4c0b7b2ac17d1b9ed575a80245c
victorsworld <victordirect48@gmail.com>
2023-07-02 17:32:22 -0400

    CreateMovie Updated with If statments on HandleSubmit

--- src/Components/EditMovie.js
+++ src/Components/EditMovie.js
@@ -20,18 +20,20 @@ const EditMovie = () => {
     setMovieData((prevMovies) => {
       const movieIndex = prevMovies.findIndex((movie) => movie.id.toString() === id);
 
+      const previousMovie = prevMovies[movieIndex];
       if (movieIndex !== -1) {
-        // Create a new movie object with the updated properties
+      
+        //Created new movie that when anything in the object is not updated will return with an iternary if statement
         const updatedMovie = {
-          id: prevMovies[movieIndex].id,
-          title: editTitle,
-          year: parseInt(editYear),
-          director: editDirector,
-          genre: editGenre,
-          rating: parseFloat(editRating),
-          runtime: parseInt(editRuntime),
-          actors: editActors,
-          plot: editPlot,
+        id: previousMovie.id,
+        title: editTitle || previousMovie.title,
+        year: editYear ? parseInt(editYear) : previousMovie.year,
+        director: editDirector || previousMovie.director,
+        genre: editGenre || previousMovie.genre,
+        rating: editRating ? parseFloat(editRating) : previousMovie.rating,
+        runtime: editRuntime ? parseInt(editRuntime) : previousMovie.runtime,
+        actors: editActors.length > 0 ? editActors : previousMovie.actors,
+        plot: editPlot || previousMovie.plot,
         };
 
         // Create a new array of movies with the updated movie
@@ -42,45 +44,77 @@ const EditMovie = () => {
 
       return prevMovies;
     });
-  }
-  
-    const handleOnDelete = () => {
-      // Using a filter array method to filter the blogs in the array by ID
-      const remainingMovies = movieData.filter((movie) => {
-        return movie.id.toString() !== id ;
-      });
-      // SetBlogs to the filtered blog
-      setMovieData(remainingMovies);
-      navigate('/');
-    };
+    navigate('/movies-list');
+  };
 
+  const handleOnDelete = () => {
+    // Using a filter array method to filter the blogs in the array by ID
+    const remainingMovies = movieData.filter((movie) => {
+      return movie.id.toString() !== id;
+    });
+    // SetBlogs to the filtered blog
+    setMovieData(remainingMovies);
+    navigate('/movies-list');
+  };
 
   return (
     <div>
       <h1>Edit Movie</h1>
       <label htmlFor="editTitle">Title:</label>
-      <input id="editTitle" value={editTitle} onChange={(e) => setEditTitle(e.target.value)} />
+      <input
+        id="editTitle"
+        value={editTitle}
+        onChange={(e) => setEditTitle(e.target.value)}
+      />
       <br />
       <label htmlFor="editYear">Year:</label>
-      <input id="editYear" value={editYear} onChange={(e) => setEditYear(e.target.value)} />
+      <input
+        id="editYear"
+        value={editYear}
+        onChange={(e) => setEditYear(e.target.value)}
+      />
       <br />
       <label htmlFor="editDirector">Director:</label>
-      <input id="editDirector" value={editDirector} onChange={(e) => setEditDirector(e.target.value)} />
+      <input
+        id="editDirector"
+        value={editDirector}
+        onChange={(e) => setEditDirector(e.target.value)}
+      />
       <br />
       <label htmlFor="editGenre">Genre:</label>
-      <input id="editGenre" value={editGenre} onChange={(e) => setEditGenre(e.target.value)} />
+      <input
+        id="editGenre"
+        value={editGenre}
+        onChange={(e) => setEditGenre(e.target.value)}
+      />
       <br />
       <label htmlFor="editRating">Rating:</label>
-      <input id="editRating" value={editRating} onChange={(e) => setEditRating(e.target.value)} />
+      <input
+        id="editRating"
+        value={editRating}
+        onChange={(e) => setEditRating(e.target.value)}
+      />
       <br />
       <label htmlFor="editRuntime">Runtime:</label>
-      <input id="editRuntime" value={editRuntime} onChange={(e) => setEditRuntime(e.target.value)} />
+      <input
+        id="editRuntime"
+        value={editRuntime}
+        onChange={(e) => setEditRuntime(e.target.value)}
+      />
       <br />
       <label htmlFor="editActors">Actors:</label>
-      <input id="editActors" value={editActors.join(', ')} onChange={(e) => setEditActors(e.target.value.split(','))} />
+      <input
+        id="editActors"
+        value={editActors.join(', ')}
+        onChange={(e) => setEditActors(e.target.value.split(','))}
+      />
       <br />
       <label htmlFor="editPlot">Plot:</label>
-      <textarea id="editPlot" value={editPlot} onChange={(e) => setEditPlot(e.target.value)} />
+      <textarea
+        id="editPlot"
+        value={editPlot}
+        onChange={(e) => setEditPlot(e.target.value)}
+      />
       <br />
       <button onClick={handleOnSubmit}>Submit</button>
       <button onClick={handleOnDelete}>Delete</button>
@@ -88,4 +122,4 @@ const EditMovie = () => {
   );
 };
 
-export default EditMovie
+export default EditMovie;
